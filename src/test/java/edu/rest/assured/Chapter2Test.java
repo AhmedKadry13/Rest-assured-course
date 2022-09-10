5fb19d670d2f686df54c561fa3ecfc65818749fb
Ahmed Kadry <ak.dev.business.360@gmail.com>
2022-09-10 12:56:06 +0200

    refactor chapter1Test and added chapter2

--- src/test/java/edu/rest/assured/Chapter2Test.java
+++ src/test/java/edu/rest/assured/Chapter2Test.java
@@ -3,19 +3,18 @@ package edu.rest.assured;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
 import static io.restassured.RestAssured.given;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 
-
-public class Chapter1Test {
+public class Chapter2Test {
 
     public String baseUrl = "https://www.zippopotam.us/us/";
 
-
     @Test
-    public void requestUsZipCode90210_checkLoaction() {
+    public void requestUsZipCode90210_checkContentType() {
 
         String urlFor90210 = baseUrl.concat("90210");
 
@@ -24,10 +23,7 @@ public class Chapter1Test {
         given()
         .when()
         .get(urlFor90210)
-        .then().assertThat().statusCode(200)
-        .and().assertThat().body("places[0].'place name'",equalTo("Beverly Hills"));
-        
+        .then().assertThat().contentType(ContentType.JSON);
     }
-
     
 }
